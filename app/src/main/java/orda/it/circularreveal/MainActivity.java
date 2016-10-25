e259a44fe25518ee97e2c6deb33100216fe70f58
Daniele Orlando <daniele.orlando@gmail.com>
2016-10-25 10:25:03 +0200

    Initial commit

---
+++ app/src/main/java/orda/it/circularreveal/MainActivity.java
@@ -0,0 +1,101 @@
+package orda.it.circularreveal;
+
+import android.animation.Animator;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private RelativeLayout layoutMain;
+    private RelativeLayout layoutButtons;
+    private RelativeLayout layoutContent;
+    private boolean isOpen = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        layoutMain = (RelativeLayout) findViewById(R.id.layoutMain);
+        layoutButtons = (RelativeLayout) findViewById(R.id.layoutButtons);
+        layoutContent = (RelativeLayout) findViewById(R.id.layoutContent);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewMenu();
+            }
+        });
+    }
+
+    private void viewMenu() {
+
+        if (!isOpen) {
+
+            int x = layoutContent.getRight();
+            int y = layoutContent.getBottom();
+
+            int startRadius = 0;
+            int endRadius = (int) Math.hypot(layoutMain.getWidth(), layoutMain.getHeight());
+
+            fab.setBackgroundTintList(ColorStateList.valueOf(ResourcesCompat.getColor(getResources(),android.R.color.white,null)));
+            fab.setImageResource(R.drawable.ic_close_grey);
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(layoutButtons, x, y, startRadius, endRadius);
+
+            layoutButtons.setVisibility(View.VISIBLE);
+            anim.start();
+
+            isOpen = true;
+
+        } else {
+
+            int x = layoutButtons.getRight();
+            int y = layoutButtons.getBottom();
+
+            int startRadius = Math.max(layoutContent.getWidth(), layoutContent.getHeight());
+            int endRadius = 0;
+
+            fab.setBackgroundTintList(ColorStateList.valueOf(ResourcesCompat.getColor(getResources(),R.color.colorAccent,null)));
+            fab.setImageResource(R.drawable.ic_plus_white);
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(layoutButtons, x, y, startRadius, endRadius);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    layoutButtons.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+            anim.start();
+
+            isOpen = false;
+        }
+    }
+}
